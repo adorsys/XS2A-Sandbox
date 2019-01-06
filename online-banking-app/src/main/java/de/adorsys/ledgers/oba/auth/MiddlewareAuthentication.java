46466b7c411d182e34942c8774e60737f9231fed
fpo <fpo@adorsys.de>
2019-01-06 09:42:44 -0500

    Fix login workflow

--- online-banking-app/src/main/java/de/adorsys/ledgers/oba/auth/MiddlewareAuthentication.java
+++ online-banking-app/src/main/java/de/adorsys/ledgers/oba/auth/MiddlewareAuthentication.java
@@ -1,18 +1,11 @@
 package de.adorsys.ledgers.oba.auth;
 
-import static org.apache.commons.lang3.StringUtils.equalsIgnoreCase;
-
 import java.util.Collection;
-import java.util.List;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 
-import de.adorsys.ledgers.middleware.api.domain.um.AccessTokenTO;
-import de.adorsys.ledgers.middleware.api.domain.um.AccessTypeTO;
-import de.adorsys.ledgers.middleware.api.domain.um.AccountAccessTO;
 import de.adorsys.ledgers.middleware.api.domain.um.BearerTokenTO;
-import de.adorsys.ledgers.middleware.api.domain.um.UserRoleTO;
 
 public class MiddlewareAuthentication extends UsernamePasswordAuthenticationToken {
 
@@ -28,49 +21,5 @@ public class MiddlewareAuthentication extends UsernamePasswordAuthenticationToke
     
     public BearerTokenTO getBearerToken() {
     	return (BearerTokenTO) getCredentials();
-    }
-    
-    public boolean checkAccountInfoAccess(String iban) {
-    	BearerTokenTO bearerToken = getBearerToken();
-    	if(bearerToken==null) {
-    		return false;
-    	}
-    	
-    	AccessTokenTO token = bearerToken.getAccessTokenObject();
-    	// Staff always have account access
-    	if(UserRoleTO.STAFF == token.getRole() || UserRoleTO.SYSTEM == token.getRole()) {
-    		return true;
-    	}
-
-    	// Customer must have explicit permission
-    	if(UserRoleTO.CUSTOMER == token.getRole()) {
-	    	List<AccountAccessTO> accountAccesses = token.getAccountAccesses();
-	    	return accountAccesses.stream()
-	    		.filter(a -> equalsIgnoreCase(iban, a.getIban()))
-	    		.findAny().isPresent();
-    	}
-    	
-    	return false;
-    }
-
-    public boolean checkPaymentInitAccess(String iban) {
-    	AccessTokenTO token = getBearerToken().getAccessTokenObject();
-    	// Customer must have explicit permission
-    	if(UserRoleTO.CUSTOMER == token.getRole()) {
-	    	List<AccountAccessTO> accountAccesses = token.getAccountAccesses();
-	    	return accountAccesses.stream()
-	    		.filter(a -> paymentAccess(a, iban))
-	    		.findAny().isPresent();
-    	}
-    	
-    	return false;
-    }
-    
-    private static boolean paymentAccess(AccountAccessTO a, String iban) {
-    	return equalsIgnoreCase(iban, a.getIban()) && 
-			(
-				AccessTypeTO.OWNER.equals(a.getAccessType()) || 
-				AccessTypeTO.DISPOSE.equals(a.getAccessType())
-			);
     }
 }
