ec909a4baa3ce906427f4933e9d198d860d3d548
fpo <fpo@adorsys.de>
2019-02-02 08:41:53 +0100

    Included modifications from xs2a 1.17-Snapshot

--- oba-rest-api/src/main/java/de/adorsys/ledgers/oba/rest/api/domain/ConsentWorkflow.java
+++ oba-rest-api/src/main/java/de/adorsys/ledgers/oba/rest/api/domain/ConsentWorkflow.java
@@ -10,7 +10,7 @@ import de.adorsys.ledgers.middleware.api.domain.sca.ScaStatusTO;
 import de.adorsys.ledgers.middleware.api.domain.um.BearerTokenTO;
 import de.adorsys.ledgers.middleware.api.domain.um.ScaUserDataTO;
 import de.adorsys.ledgers.oba.rest.api.consentref.ConsentReference;
-import de.adorsys.psd2.consent.api.ais.CmsAisConsentResponse;
+import de.adorsys.psd2.consent.psu.api.ais.CmsAisConsentResponse;
 
 public class ConsentWorkflow {
 	private final CmsAisConsentResponse consentResponse;
