25932cd1e11df149f145573c969414cef58987b2
Vladimir Nechai <vne@adorsys.de>
2019-03-04 19:56:52 +0100

    components init

---
+++ oba-ui/src/app/ais/consent/tan-confirmation/tan-confirmation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tan-confirmation',
+  templateUrl: './tan-confirmation.component.html',
+  styleUrls: ['./tan-confirmation.component.scss']
+})
+export class TanConfirmationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
