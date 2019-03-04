b355bb951e1b5e91c156dba31888e04c1c7fcb9d
Vladimir Nechai <vne@adorsys.de>
2019-03-04 19:56:52 +0100

    components init

---
+++ oba-ui/src/app/ais/consent/bank-offered/bank-offered.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bank-offered',
+  templateUrl: './bank-offered.component.html',
+  styleUrls: ['./bank-offered.component.scss']
+})
+export class BankOfferedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ oba-ui/src/app/result-page/result-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-result-page',
+  templateUrl: './result-page.component.html',
+  styleUrls: ['./result-page.component.scss']
+})
+export class ResultPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
