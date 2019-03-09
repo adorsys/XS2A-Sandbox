11c5fe205801fbae2a0a78973508d8e6f20c32e0
gta <gerard.talla@parcaune.com>
2019-03-09 14:27:31 +0100

    1) Add bootstrap, 2) Loading API by using the swagger-file(JSON-File), 3) Installing 'Puppeteer' to run Tests on 'Headless Chromium‘, 4) karma.config.js => Modification of ChromeHeadless object, 5) Set proxy.conf.json => To set the hostname in localhost when calling HttpRequest, 6) Add account details component, 7) add routing module, 8) write tests for AppComponent and RoutingComponent, 9) ReadMe update

--- tpp-ui/src/app/uploadFile/uploadFile.component.ts
+++ tpp-ui/src/app/uploadFile/uploadFile.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {FileItem} from 'ng2-file-upload';
-import {environment} from '../../environments/environment';
 import {UploadOptions} from '../services/upload.service';
 
 @Component({
@@ -11,7 +10,6 @@ import {UploadOptions} from '../services/upload.service';
 })
 export class UploadFileComponent implements OnInit {
 
-    private url = `${environment.staffAccessResourceEndPoint}`;
     public options: UploadOptions;
 
     constructor(private router: Router) {
