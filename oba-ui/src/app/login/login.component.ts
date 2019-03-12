4a71b597001cc7dbc079f82e52118bd072d3588b
Hergy Tchuinkou <htc@adorsys.de>
2019-03-12 15:36:14 +0100

    refactor login and app component

--- oba-ui/src/app/login/login.component.ts
+++ oba-ui/src/app/login/login.component.ts
@@ -6,6 +6,8 @@ import {RoutingPath} from "../common/models/routing-path.model";
 import {AisService} from "../common/services/ais.service";
 import {URL_PARAMS_PROVIDER} from "../common/constants/constants";
 import {HttpHeaders} from "@angular/common/http";
+import {BehaviorSubject} from "rxjs/internal/BehaviorSubject";
+import {ObaUtils} from "../common/utils/oba-utils";
 
 @Component({
     selector: 'ais-login',
@@ -13,6 +15,7 @@ import {HttpHeaders} from "@angular/common/http";
     styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
     loginForm: FormGroup;
     loading = false;
     error: string;
@@ -20,6 +23,7 @@ export class LoginComponent implements OnInit {
     encryptedConsentId: string;
     Cookie = 'eyJraWQiOiJNQmxEd3d1NlJ4RXJ1dGhmc0QycnBZIiwiYWxnIjoiSFMyNTYifQ.eyJlbmMtY29uc2VudC1pZCI6IlA1a1p1bTZpMFZYT3E5bnNFNmcxaVNXT3RPTUxnd2c1OUtIdThVRm9ZcUJTYmRtMnR3UUhfTEJURzk0bFRJNnVTZmdpcWxER0VmWWI3aFhPeXpfUDlnPT1fPV9iUzZwNlh2VFdJIiwiY29uc2VudC10eXBlIjoiQUlTIiwicmVkaXJlY3QtaWQiOiI1ODc0YTQ4NC01NWUwLTRmNTQtYTYzYS0zN2JkNjg3YjAzMDEiLCJleHAiOjE1NTIzOTE2MDgsImlhdCI6MTU1MjM5MTMwOCwianRpIjoiX0pvX3VGaHFRdW92M0doWEZBaHV2dyJ9.4aJTNRHGg4gZGFcAlIVo-MizWwmotSik5Opf-a3I84w';
     header = new HttpHeaders();
+ //   input: string;
 
     constructor(private formBuilder: FormBuilder,
                 private router: Router,
@@ -29,6 +33,7 @@ export class LoginComponent implements OnInit {
         this.encryptedConsentId = params['encryptedConsentId'];
         this.authorisationId = params['authorisationId'];
         console.log('params: ', params);
+ //       this.input = '/login', ObaUtils.getQueryParams(this.encryptedConsentId, this.authorisationId);
     }
 
     ngOnInit() {
@@ -36,21 +41,21 @@ export class LoginComponent implements OnInit {
             login: ['', Validators.required],
             pin: ['', Validators.required]
         });
-        this.header.append("Cookie", this.Cookie);
     }
 
     onSubmit() {
         this.loading = true;
+        console.log('loginUsingAuthorizationId');
         this.psuAisService.loginUsingAuthorizationId({
             ...this.loginForm.value,
             encryptedConsentId: this.encryptedConsentId,
             authorisationId: this.authorisationId,
-            headers: this.header
+            Cookie: this.Cookie
         })
             .pipe(first())
             .subscribe(
                 data => {
-                    this.router.navigate([`./${RoutingPath.BANK_OFFERED}`]);
+                    this.router.navigate(['/login', ObaUtils.getQueryParams(this.encryptedConsentId, this.authorisationId)]);
                 },
                 error => {
                     this.error = error;
