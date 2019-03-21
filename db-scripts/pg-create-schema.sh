078a832a13798f76c808e1dfa4a4e4d4c2c24d21
Petro Rudenko <pru@adorsys.com.ua>
2019-03-21 15:06:58 +0200

    added db script

---
+++ db-scripts/pg-create-schema.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# create consent schema and give permissions to cms user. Needed for
+# docker-compose so  that we can start the DB with the schema already being present
+set -e
+echo "Create schema='consent' for local postgres installation"
+psql -U postgres -d consent -c 'CREATE SCHEMA IF NOT EXISTS consent AUTHORIZATION cms;'
+
+echo "Create schema='ledgers' for local postgres installation"
+psql -U postgres -d consent -c 'CREATE SCHEMA IF NOT EXISTS ledgers AUTHORIZATION cms;'
