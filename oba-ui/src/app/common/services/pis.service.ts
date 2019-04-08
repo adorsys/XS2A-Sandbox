091e39f460db111fa18b54e2e9d80ce14db7f7f6
Vladimir Nechai <vne@adorsys.de>
2019-04-08 15:01:09 +0200

    select sca method function

--- oba-ui/src/app/common/services/pis.service.ts
+++ oba-ui/src/app/common/services/pis.service.ts
@@ -1,17 +1,19 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {PSUPISService} from "../../api/services";
 import {Observable} from "rxjs";
 import {AuthorizeResponse} from "../../api/models/authorize-response";
 import PisAuthUsingGETParams = PSUPISService.PisAuthUsingGETParams;
 import {PaymentAuthorizeResponse} from "../../api/models";
 import LoginUsingPOST1Params = PSUPISService.LoginUsingPOST1Params;
+import SelectMethodUsingPOST1Params = PSUPISService.SelectMethodUsingPOST1Params;
 
 @Injectable({
   providedIn: 'root'
 })
 export class PisService {
 
-  constructor(private pisService: PSUPISService) { }
+  constructor(private pisService: PSUPISService) {
+  }
 
   public pisAuthCode(params: PisAuthUsingGETParams): Observable<AuthorizeResponse> {
     return this.pisService.pisAuthUsingGET(params);
@@ -19,5 +21,9 @@ export class PisService {
 
   public pisLogin(params: LoginUsingPOST1Params): Observable<PaymentAuthorizeResponse> {
     return this.pisService.loginUsingPOST1(params);
+  }
+
+  public selectScaMethod(params: SelectMethodUsingPOST1Params): Observable<PaymentAuthorizeResponse> {
+    return this.pisService.selectMethodUsingPOST1(params);
   }
 }
