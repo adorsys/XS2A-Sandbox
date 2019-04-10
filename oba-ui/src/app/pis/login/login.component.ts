78808cf2ffa631622781d85a995cd0673ee309f3
Vladimir Nechai <vne@adorsys.de>
2019-04-11 00:39:26 +0200

    login for pis

---
+++ oba-ui/src/app/pis/login/login.component.ts
@@ -0,0 +1,85 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Subscription} from "rxjs";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ShareDataService} from "../../common/services/share-data.service";
+import {RoutingPath} from "../../common/models/routing-path.model";
+import {PisService} from "../../common/services/pis.service";
+import {PSUPISService} from "../../api/services/psupis.service";
+import LoginUsingPOST2Params = PSUPISService.LoginUsingPOST2Params;
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit, OnDestroy {
+
+  loginForm: FormGroup;
+  invalidCredentials: boolean;
+
+  private encryptedPaymentId: string;
+  private redirectId: string;
+
+  private subscriptions: Subscription[] = [];
+
+
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private activatedRoute: ActivatedRoute,
+              private shareService: ShareDataService,
+              private pisService: PisService) {
+  }
+
+  ngOnInit() {
+    this.initLoginForm();
+
+    this.getPisAuthCode();
+  }
+
+  public onSubmit(): void {
+    this.subscriptions.push(
+      this.pisService.pisLogin({
+        ...this.loginForm.value,
+        encryptedPaymentId: this.encryptedPaymentId,
+        authorisationId: this.redirectId,
+      } as LoginUsingPOST2Params).subscribe(authorisationResponse => {
+        console.log(authorisationResponse);
+        this.shareService.changeData(authorisationResponse);
+        this.router.navigate([`${RoutingPath.PAYMENT_INITIATION}/${RoutingPath.GRANT_CONSENT}`]);
+      }, (error) => {
+        console.log(error);
+        this.invalidCredentials = true;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
+
+  private getPisAuthCode(): void {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.encryptedPaymentId = params['paymentId'];
+      this.redirectId = params['redirectId'];
+
+      this.subscriptions.push(
+        this.pisService.pisAuthCode({encryptedPaymentId: this.encryptedPaymentId, redirectId: this.redirectId})
+          .subscribe(authCodeResponse => {
+              this.shareService.changeData(authCodeResponse);
+            },
+            (error) => {
+              console.log(error);
+            })
+      );
+    })
+  }
+
+  private initLoginForm(): void {
+    this.loginForm = this.formBuilder.group({
+      login: ['', Validators.required],
+      pin: ['', Validators.required]
+    });
+  }
+
+}
