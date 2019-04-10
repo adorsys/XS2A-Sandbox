68be9fbecce7642381bcd84cb025a041e0db85bc
Vladimir Nechai <vne@adorsys.de>
2019-04-11 00:26:36 +0200

    add pis module

--- oba-ui/src/app/common/models/routing-path.model.ts
+++ oba-ui/src/app/common/models/routing-path.model.ts
@@ -1,9 +1,11 @@
 export  enum RoutingPath {
     ACCOUNT_INFORMATION = 'account-information',
+    PAYMENT_INITIATION = 'payment-initiation',
     LOGIN = 'login',
     RESULT = 'result',
     BANK_OFFERED = 'bank-offered',
     GRANT_CONSENT = 'grant-consent',
+    CONFIRM_PAYMENT = 'grant-consent',
     SELECT_SCA = 'select-sca',
     TAN_CONFIRMATION = 'tan-confirm'
 
---
+++ oba-ui/src/app/pis/pis.module.ts
@@ -0,0 +1,30 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {PisRoutingModule} from './pis-routing.module';
+import {LoginComponent} from './login/login.component';
+import {TanConfirmationComponent} from './tan-confirmation/tan-confirmation.component';
+import {ResultPageComponent} from './result-page/result-page.component';
+import {SelectScaComponent} from './select-sca/select-sca.component';
+import {PaymentDetailsComponent} from './payment-details/payment-details.component';
+import {ConfirmPaymentComponent} from './confirm-payment/confirm-payment.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    TanConfirmationComponent,
+    ResultPageComponent,
+    SelectScaComponent,
+    PaymentDetailsComponent,
+    ConfirmPaymentComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    PisRoutingModule
+  ]
+})
+export class PisModule {
+}
