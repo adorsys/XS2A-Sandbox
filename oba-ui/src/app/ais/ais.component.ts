9072d3855da1926827289b0f644569c1341f8a88
Vladimir Nechai <vne@adorsys.de>
2019-04-10 18:52:56 +0200

    add ais component

---
+++ oba-ui/src/app/ais/ais.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ais',
+  templateUrl: './ais.component.html',
+  styleUrls: ['./ais.component.scss']
+})
+export class AisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
