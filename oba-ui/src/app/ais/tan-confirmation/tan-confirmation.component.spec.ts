48c4b1183b6abd67705e36d531b30b6763f1f2bd
Vladimir Nechai <vne@adorsys.de>
2019-04-11 00:01:15 +0200

    tan confirmation

---
+++ oba-ui/src/app/ais/tan-confirmation/tan-confirmation.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TanConfirmationComponent } from './tan-confirmation.component';
+
+describe('TanConfirmationComponent', () => {
+  let component: TanConfirmationComponent;
+  let fixture: ComponentFixture<TanConfirmationComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TanConfirmationComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TanConfirmationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
