22ccd37cac393a8d9e78fb5f7ecfb452ea423b49
Vladimir Nechai <vne@adorsys.de>
2019-04-11 02:22:54 +0200

    pis cancel login

--- oba-ui/src/app/common/models/routing-path.model.ts
+++ oba-ui/src/app/common/models/routing-path.model.ts
@@ -1,13 +1,14 @@
 export  enum RoutingPath {
     ACCOUNT_INFORMATION = 'account-information',
     PAYMENT_INITIATION = 'payment-initiation',
+    PAYMENT_CANCELLATION = 'payment-cancellation',
     LOGIN = 'login',
     RESULT = 'result',
     BANK_OFFERED = 'bank-offered',
     GRANT_CONSENT = 'grant-consent',
     CONFIRM_PAYMENT = 'confirm-payment',
+    CONFIRM_CANCELLATION = 'confirm-cancellation',
     SELECT_SCA = 'select-sca',
     TAN_CONFIRMATION = 'tan-confirm',
     ERROR_404 = 'tan-confirm'
-
 }
