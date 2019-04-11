6114f5bb872c87a01d4e54b6875167ae57b34aae
Vladimir Nechai <vne@adorsys.de>
2019-04-11 02:24:36 +0200

    pis cancellation payment details

---
+++ oba-ui/src/app/payment-cancellation/payment-details/payment-details.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {Subscription} from "rxjs";
+import {PaymentAuthorizeResponse} from "../../api/models/payment-authorize-response";
+import {ShareDataService} from "../../common/services/share-data.service";
+
+@Component({
+  selector: 'app-payment-details',
+  templateUrl: './payment-details.component.html',
+  styleUrls: ['./payment-details.component.scss']
+})
+export class PaymentDetailsComponent implements OnInit {
+
+  private subscriptions: Subscription[] = [];
+
+  public authResponse: PaymentAuthorizeResponse;
+
+  constructor(private sharedService: ShareDataService) {}
+
+  ngOnInit() {
+    this.sharedService.currentData.subscribe(
+      authResponse => this.authResponse = authResponse
+    );
+  }
+
+}
