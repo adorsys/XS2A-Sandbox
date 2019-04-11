8d2727d8ff5201173169853d1fba22747f4989b8
Vladimir Nechai <vne@adorsys.de>
2019-04-11 02:32:54 +0200

    sca select cancellation

--- oba-ui/src/app/app.module.ts
+++ oba-ui/src/app/app.module.ts
@@ -2,20 +2,13 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {ErrorHandler, NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
-
-import {AccountDetailsComponent} from './account-details/account-details.component';
-import {BankOfferedComponent} from './bank-offered/bank-offered.component';
-import {ScaSelectionComponent} from './sca-selection/sca-selection.component';
-import {TanConfirmationComponent} from './tan-confirmation/tan-confirmation.component';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {AuthInterceptor} from './common/interceptors/AuthInterceptor';
 import {AisService} from './common/services/ais.service';
 import {ShareDataService} from './common/services/share-data.service';
-import {ResultPageComponent} from './result-page/result-page.component';
 import {ObaErrorsHandler} from "./common/interceptors/ObaErrorsHandler";
 import {NotFoundComponent} from './not-found/not-found.component';
-import {LoginComponent} from "./login/login.component";
 import {NgHttpLoaderModule} from "ng-http-loader";
 
 @NgModule({
