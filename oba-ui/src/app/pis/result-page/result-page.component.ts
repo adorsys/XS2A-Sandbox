98c69e11caf86004a191c469d0813c646738d561
Vladimir Nechai <vne@adorsys.de>
2019-04-11 02:04:46 +0200

    pis result page

---
+++ oba-ui/src/app/pis/result-page/result-page.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import {ConsentAuthorizeResponse} from "../../api/models/consent-authorize-response";
+import {ActivatedRoute, Router} from "@angular/router";
+import {AisService} from "../../common/services/ais.service";
+import {ShareDataService} from "../../common/services/share-data.service";
+import {PaymentAuthorizeResponse} from "../../api/models";
+
+@Component({
+  selector: 'app-result-page',
+  templateUrl: './result-page.component.html',
+  styleUrls: ['./result-page.component.scss']
+})
+export class ResultPageComponent implements OnInit {
+
+  public authResponse: PaymentAuthorizeResponse;
+  public scaStatus: string;
+  public ref: string;
+
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private aisService: AisService,
+              private shareService: ShareDataService) {
+  }
+
+  public ngOnInit(): void {
+    // get query params and build link
+    this.route.queryParams.subscribe(params => {
+      // TODO: use routerlink to build a link https://git.adorsys.de/adorsys/xs2a/psd2-dynamic-sandbox/issues/8
+      this.ref = 'http://localhost:8090/ais/' + params['encryptedConsentId'] +
+        '/authorisation/' + params['authorisationId'] +
+        '/done?backToTpp=true&forgetConsent=true';
+    });
+
+    // get consent data from shared service
+    this.shareService.currentData.subscribe(data => {
+      if (data) {
+        this.shareService.currentData.subscribe(authResponse => {
+          this.authResponse = authResponse;
+          this.scaStatus = this.authResponse.scaStatus;
+        });
+      }
+    });
+  }
+
+  public backToTpp(): void {
+
+  }
+
+  public forgetConsent(): void {
+
+  }
+
+  ngOnDestroy(): void {
+  }
+
+}
