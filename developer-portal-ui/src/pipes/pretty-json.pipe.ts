6e974a1eaed3afae283613cc1db7d3fab482a163
Mykhailo Pugovkin <mykhailo-pugovkin@dev-optima.com>
2019-05-20 16:20:34 +0300

    #87 add pipe for copied code for good look in firefox

---
+++ developer-portal-ui/src/pipes/pretty-json.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'prettyJson',
+})
+export class PrettyJsonPipe implements PipeTransform {
+  transform(value: any, args?: any): any {
+    return JSON.stringify(value, null, 4);
+  }
+}
