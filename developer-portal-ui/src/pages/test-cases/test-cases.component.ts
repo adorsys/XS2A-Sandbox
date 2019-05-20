0f63462edfd936bc8371c8f50081de201bf42b85
Mykhailo Pugovkin <mykhailo-pugovkin@dev-optima.com>
2019-05-20 16:18:41 +0300

    #85 add collapse for redirect and embedded menus

--- developer-portal-ui/src/pages/test-cases/test-cases.component.ts
+++ developer-portal-ui/src/pages/test-cases/test-cases.component.ts
@@ -8,6 +8,9 @@ import { DataService } from '../../services/data.service';
   styleUrls: ['./test-cases.component.scss'],
 })
 export class TestCasesComponent implements OnInit {
+  redirectFlag = false;
+  embeddedFlag = false;
+
   constructor(
     private router: Router,
     public dataService: DataService,
@@ -19,7 +22,32 @@ export class TestCasesComponent implements OnInit {
   }
 
   onActivate(ev) {
-    this.dataService.currentRouteUrl = this.actRoute['_routerState'].snapshot.url;
+    this.dataService.currentRouteUrl = this.actRoute[
+      '_routerState'
+    ].snapshot.url;
+  }
+
+  collapseThis(collapseId: string): void {
+    const collapsibleItemContent = document.getElementById(
+      `${collapseId}-content`
+    );
+
+    switch (collapseId) {
+      case 'redirect':
+        this.redirectFlag = !this.redirectFlag;
+        break;
+      case 'embedded':
+        this.embeddedFlag = !this.embeddedFlag;
+        break;
+    }
+
+    if (collapsibleItemContent.style.maxHeight) {
+      collapsibleItemContent.style.maxHeight = '';
+    } else {
+      collapsibleItemContent.style.maxHeight = `${
+        collapsibleItemContent.scrollHeight
+      }px`;
+    }
   }
 
   ngOnInit() {}
