ea15ec08d20882dfd56132f8266cdb349d99d185
Volodymyr Nechai <vne@adorsys.de>
2019-05-29 10:16:02 +0000

    excessive slash

--- developer-portal-ui/src/services/rest.service.ts
+++ developer-portal-ui/src/services/rest.service.ts
@@ -5,29 +5,30 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class RestService {
   // serverUrl = 'https://xs2a.integ.cloud.adorsys.de/v1/consents';
-  serverUrl = '/xs2a-proxy/';
+  serverUrl = '/xs2a-proxy';
 
   constructor(private http: HttpClient) {}
 
   public sendRequest(method, url, headerParams, body?): Observable<any> {
+    console.log(this.serverUrl + url);
     switch (method) {
       case 'POST':
-        return this.http.post(this.serverUrl + url, body, {
+        return this.http.post(this.serverUrl + '/' + url, body, {
           observe: 'response',
           headers: new HttpHeaders(headerParams),
         });
       case 'GET':
-        return this.http.get(this.serverUrl + url, {
+        return this.http.get(this.serverUrl + '/' + url, {
           observe: 'response',
           headers: new HttpHeaders(headerParams),
         });
       case 'PUT':
-        return this.http.put(this.serverUrl + url, body, {
+        return this.http.put(this.serverUrl + '/' + url, body, {
           observe: 'response',
           headers: new HttpHeaders(headerParams),
         });
       case 'DELETE':
-        return this.http.delete(this.serverUrl + url, {
+        return this.http.delete(this.serverUrl + '/' + url, {
           observe: 'response',
           headers: new HttpHeaders(headerParams),
         });
