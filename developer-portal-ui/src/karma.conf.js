92fe624c7c56cf18fab3f325d49a4eb1a45b8dcb
koaex <kan@adorsys.com.ua>
2019-08-07 13:14:18 +0300

    reconfig developer portal karma config

--- developer-portal-ui/src/karma.conf.js
+++ developer-portal-ui/src/karma.conf.js
@@ -26,6 +26,11 @@ module.exports = function(config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeCI: {
+        base: 'ChromeHeadless',
+      },
+    },
     singleRun: false,
     restartOnFileChange: true,
   });
