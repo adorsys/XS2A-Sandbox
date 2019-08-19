8735173b3c86f521a535bcb6c0895bf9aa9711a0
Petro Rudenko <pru@adorsys.com.ua>
2019-08-19 14:01:02 +0000

    forget password oba ui

--- online-banking/online-banking-app/src/main/java/de/adorsys/ledgers/oba/rest/server/resource/oba/ObaAuthorizationApiController.java
+++ online-banking/online-banking-app/src/main/java/de/adorsys/ledgers/oba/rest/server/resource/oba/ObaAuthorizationApiController.java
@@ -1,8 +1,13 @@
 package de.adorsys.ledgers.oba.rest.server.resource.oba;
 
+import de.adorsys.ledgers.middleware.client.rest.ResetPasswordRestClient;
 import de.adorsys.ledgers.oba.rest.api.resource.oba.ObaAuthorizationApi;
+import de.adorsys.ledgers.security.ResetPassword;
+import de.adorsys.ledgers.security.SendCode;
+import de.adorsys.ledgers.security.UpdatePassword;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(ObaAuthorizationApi.BASE_PATH)
 @RequiredArgsConstructor
 public class ObaAuthorizationApiController implements ObaAuthorizationApi {
+    private final ResetPasswordRestClient resetPasswordRestClient;
 
     @Override
     public void login(String login, String password) {}
+
+    @Override
+    public ResponseEntity<SendCode> sendCode(ResetPassword resetPassword) {
+        return resetPasswordRestClient.sendCode(resetPassword);
+    }
+
+    @Override
+    public ResponseEntity<UpdatePassword> updatePassword(ResetPassword resetPassword) {
+        return resetPasswordRestClient.updatePassword(resetPassword);
+    }
 }
