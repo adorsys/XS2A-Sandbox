50a0ad951c1c5e952c1619c779e486e699e4c577
Dmytro Mishchuk <dmi@adorsys.com.ua>
2019-10-29 10:55:48 +0000

    fixed scan base packages in tpp app

--- tpp-app/tpp-rest-server/src/main/java/de/adorsys/psd2/sandbox/tpp/rest/server/TppApplication.java
+++ tpp-app/tpp-rest-server/src/main/java/de/adorsys/psd2/sandbox/tpp/rest/server/TppApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients(basePackageClasses = {UserMgmtStaffRestClient.class, UserMgmtRestClient.class}, defaultConfiguration = TppUiBeFeignConfiguration.class)
-@SpringBootApplication(scanBasePackages = "de.adorsys.psd2.mapper")
+@SpringBootApplication(scanBasePackages = {"de.adorsys.psd2.mapper", "de.adorsys.psd2.sandbox.tpp.rest.server"})
 public class TppApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(TppApplication.class).run(args);
