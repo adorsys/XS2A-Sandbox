2b6c8d279cb57a86297e559f40092837121e903d
Petro Rudenko <pru@adorsys.com.ua>
2020-02-26 16:14:44 +0000

    tpp ui demo for payment initiation

---
+++ tpp-ui-demo/js/urlConfig.js
@@ -0,0 +1,4 @@
+configs = {
+    localhost: "http://localhost:8089/v1/payments/sepa-credit-transfers",
+    xs2a: "https://dev-dynamicsandbox-xs2a.cloud.adorsys.de/api/v1/payments/sepa-credit-transfers"
+};
