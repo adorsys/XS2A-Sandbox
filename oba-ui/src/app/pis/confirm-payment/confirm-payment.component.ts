2dc09d07b5f58cddde769942cbff501991c1fbcc
Christian <noubichristian@yahoo.fr>
2020-03-11 16:00:46 +0100

    OBA-UI payment without sca Method#311

--- oba-ui/src/app/pis/confirm-payment/confirm-payment.component.ts
+++ oba-ui/src/app/pis/confirm-payment/confirm-payment.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { PaymentAuthorizeResponse } from '../../api/models';
 
 import { AccountDetailsTO } from '../../api/models/account-details-to';
 import { ConsentAuthorizeResponse } from '../../api/models/consent-authorize-response';
@@ -14,12 +15,13 @@ import { ShareDataService } from '../../common/services/share-data.service';
 })
 export class ConfirmPaymentComponent implements OnInit, OnDestroy {
 
+  public payAuthResponse: PaymentAuthorizeResponse;
   public authResponse: ConsentAuthorizeResponse;
   public encryptedConsentId: string;
   public authorisationId: string;
-
+  public transactionStatus: string;
   private subscriptions: Subscription[] = [];
-
+  private oauth2Param: boolean;
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
@@ -37,12 +39,34 @@ export class ConfirmPaymentComponent implements OnInit, OnDestroy {
             this.authResponse = authResponse;
           });
         }
-      })
+      }),
     );
-  }
+      this.shareService.currentData.subscribe(data => {
+          if (data) {
+              this.shareService.currentData.subscribe(payAuthResponse => {
+                  this.payAuthResponse = payAuthResponse;
+                  this.transactionStatus = this.payAuthResponse.payment.transactionStatus
+              });
+          }
+      });
+      // fetch oauth param value
+      this.shareService.oauthParam.subscribe((oauth2: boolean) => {
+          this.oauth2Param = oauth2;
+      });
+}
 
   public onConfirm() {
-    this.router.navigate([`${RoutingPath.PAYMENT_INITIATION}/${RoutingPath.SELECT_SCA}`]);
+      if (this.transactionStatus === "ACSP") {
+          this.router.navigate([`${RoutingPath.PAYMENT_INITIATION}/${RoutingPath.RESULT}`], {
+              queryParams: {
+                  encryptedConsentId: this.payAuthResponse.encryptedConsentId,
+                  authorisationId: this.payAuthResponse.authorisationId,
+                  oauth2: this.oauth2Param
+              }
+          })
+      } else {
+          this.router.navigate([`${RoutingPath.PAYMENT_INITIATION}/${RoutingPath.SELECT_SCA}`]);
+      }
   }
 
   public onCancel(): void {
