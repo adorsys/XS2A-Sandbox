b70af0c2767b62fc2bf5bf2e59866167323284f9
rain2581 <kan@adorsys.com.ua>
2020-04-06 12:55:19 +0300

    update postman tests

--- postman-scripts/postman_tests_local.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Run postman tests"
-newman run postman_collection.json -e postman_environment_local.json -g postman_global_variable.json --reporters cli,html --reporter-html-template html_report_template.hbs --reporter-html-export postman-tests-report.html
