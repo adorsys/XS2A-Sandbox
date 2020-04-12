2db794b06ea05d3cfe0b16885ddd32167ef5d6a4
Christian <noubichristian@yahoo.fr>
2020-04-12 18:58:41 +0200

    OBA-UI Unit test part1#603

--- oba-ui/src/app/oba/accounts/accounts.component.spec.ts
+++ oba-ui/src/app/oba/accounts/accounts.component.spec.ts
@@ -1,13 +1,14 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { OnlineBankingService } from '../../common/services/online-banking.service';
 import { AuthService } from '../../common/services/auth.service';
 import { AccountsComponent } from './accounts.component';
-
+import { of } from 'rxjs';
 describe('AccountsComponent', () => {
   let component: AccountsComponent;
   let fixture: ComponentFixture<AccountsComponent>;
+  let onlineBankingService: OnlineBankingService
   const authServiceSpy = jasmine.createSpyObj('AuthService', ['getAuthorizedUser', 'isLoggedIn', 'logout']);
 
   beforeEach(async(() => {
@@ -22,10 +23,35 @@ describe('AccountsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AccountsComponent);
     component = fixture.componentInstance;
+    onlineBankingService = TestBed.get(OnlineBankingService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+    it('should get the accounts list', () => {
+      let mockAccounts = [{
+          id: '123456',
+          iban: 'DE35653635635663',
+          bban: 'BBBAN',
+          pan: 'pan',
+          maskedPan: 'maskedPan',
+          currency: 'EUR',
+          msisdn: 'MSISDN',
+          name: 'Pupkin',
+          product: 'Deposit',
+          accountType: 'CASH',
+          accountStatus: 'ENABLED',
+          bic: 'BIChgdgd',
+          usageType: 'PRIV',
+          details: '',
+          linkedAccounts: '',
+          balances: []
+      }];
+        let accountsSpy = spyOn(onlineBankingService, 'getAccounts').and.returnValue(of({mockAccounts}));
+        component.getAccounts();
+        expect(accountsSpy).toHaveBeenCalled();
+    });
 });
--- oba-ui/src/app/oba/login/login.component.spec.ts
+++ oba-ui/src/app/oba/login/login.component.spec.ts
@@ -1,18 +1,20 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RouterTestingModule } from '@angular/router/testing';
-
-import { InfoModule } from '../../common/info/info.module';
-import { LoginComponent } from './login.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ReactiveFormsModule} from '@angular/forms';
+import {RouterTestingModule} from '@angular/router/testing';
+import {InfoModule} from '../../common/info/info.module';
+import {LoginComponent} from './login.component';
+import {AuthService} from '../../common/services/auth.service';
+import {of, throwError} from 'rxjs';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-
+  let authService: AuthService;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [ReactiveFormsModule, RouterTestingModule, InfoModule],
-      declarations: [LoginComponent]
+      declarations: [LoginComponent],
+      providers: [AuthService]
     })
       .compileComponents();
   }));
@@ -21,9 +23,22 @@ describe('LoginComponent', () => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    authService = TestBed.get(AuthService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+    it('should call the onsubmit', () => {
+        let loginSpy = spyOn(authService, 'login').and.returnValue(of({success: true}));
+        component.onSubmit();
+        expect(loginSpy).toHaveBeenCalled();
+    });
+
+    it('should throw error 401', () => {
+        let errorSpy = spyOn(authService, 'login').and.returnValue(throwError({ success: false }));
+        component.onSubmit();
+        expect(errorSpy).toHaveBeenCalled();
+    });
 });
