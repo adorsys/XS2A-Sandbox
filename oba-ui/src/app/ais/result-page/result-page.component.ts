aecd8c0a5426758aa5fe30f4aa039d9337c0db17
Dmytro Mishchuk <dmi@adorsys.com.ua>
2020-09-18 19:01:41 +0300

    fixed returning oauth code

--- oba-ui/src/app/ais/result-page/result-page.component.ts
+++ oba-ui/src/app/ais/result-page/result-page.component.ts
@@ -1,61 +1,63 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
-import {ConsentAuthorizeResponse} from '../../api/models/consent-authorize-response';
-import {AisService} from '../../common/services/ais.service';
-import {SettingsService} from '../../common/services/settings.service';
-import {ShareDataService} from '../../common/services/share-data.service';
+import { ConsentAuthorizeResponse } from '../../api/models/consent-authorize-response';
+import { AisService } from '../../common/services/ais.service';
+import { SettingsService } from '../../common/services/settings.service';
+import { ShareDataService } from '../../common/services/share-data.service';
 
 @Component({
   selector: 'app-result-page',
   templateUrl: './result-page.component.html',
-  styleUrls: ['./result-page.component.scss']
+  styleUrls: ['./result-page.component.scss'],
 })
 export class ResultPageComponent implements OnInit, OnDestroy {
-
   public authResponse: ConsentAuthorizeResponse;
   public scaStatus: string;
   public ref: string;
   public devPortalLink: string;
 
-  constructor(private router: Router,
-              private route: ActivatedRoute,
-              private aisService: AisService,
-              private settingService: SettingsService,
-              private shareService: ShareDataService) {
-  }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private aisService: AisService,
+    private settingService: SettingsService,
+    private shareService: ShareDataService
+  ) {}
 
   public ngOnInit(): void {
     // get dev portal link
-    this.devPortalLink = this.settingService.settings.devPortalUrl + '/test-cases/redirect-consent-post';
+    this.devPortalLink =
+      this.settingService.settings.devPortalUrl +
+      '/test-cases/redirect-consent-post';
 
     // Manual redirect is used because of the CORS error otherwise
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       let oauth2 = params.oauth2;
-
-      if (oauth2 === undefined || typeof oauth2 !== "boolean") {
+      if (oauth2 === undefined || oauth2 !== 'true') {
         oauth2 = false;
       }
 
-      this.ref = `/oba-proxy/ais/${params.encryptedConsentId}/authorisation/${params.authorisationId}` +
+      this.ref =
+        `/oba-proxy/ais/${params.encryptedConsentId}/authorisation/${params.authorisationId}` +
         `/done?oauth2=${oauth2}`;
     });
 
     // get consent data from shared service
-    this.shareService.currentData.subscribe(data => {
+    this.shareService.currentData.subscribe((data) => {
       if (data) {
-        this.shareService.currentData.subscribe(authResponse => {
+        this.shareService.currentData.subscribe((authResponse) => {
           this.authResponse = authResponse;
           this.scaStatus = this.authResponse.scaStatus;
           if (authResponse.authConfirmationCode) {
-            this.ref = this.ref + `&authConfirmationCode=${authResponse.authConfirmationCode}`;
+            this.ref =
+              this.ref +
+              `&authConfirmationCode=${authResponse.authConfirmationCode}`;
           }
         });
       }
     });
   }
 
-  ngOnDestroy(): void {
-  }
-
+  ngOnDestroy(): void {}
 }
