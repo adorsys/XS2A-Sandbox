d6fd8bc9af3c97a49ed6e26d109ccf0fac5414ad
Dmytro Mishchuk <dmi@adorsys.com.ua>
2020-09-18 14:38:14 +0300

    fixed test

--- online-banking/oba-rest-server/src/test/java/de/adorsys/ledgers/oba/rest/server/auth/JWTAuthenticationFilterTest.java
+++ online-banking/oba-rest-server/src/test/java/de/adorsys/ledgers/oba/rest/server/auth/JWTAuthenticationFilterTest.java
@@ -2,6 +2,7 @@ package de.adorsys.ledgers.oba.rest.server.auth;
 
 import de.adorsys.ledgers.middleware.api.domain.um.AccessTokenTO;
 import de.adorsys.ledgers.middleware.api.domain.um.BearerTokenTO;
+import de.adorsys.ledgers.middleware.client.rest.AuthRequestInterceptor;
 import de.adorsys.ledgers.oba.service.api.domain.UserAuthentication;
 import de.adorsys.ledgers.oba.service.api.service.TokenAuthenticationService;
 import org.junit.jupiter.api.Test;
@@ -39,6 +40,9 @@ class JWTAuthenticationFilterTest {
     @Mock
     private TokenAuthenticationService tokenAuthenticationService;
 
+    @Mock
+    private AuthRequestInterceptor authInterceptor;
+
     @Test
     void doFilterInternal() throws ServletException, IOException {
         // Given
@@ -68,6 +72,6 @@ class JWTAuthenticationFilterTest {
     private BearerTokenTO getBearer() {
         AccessTokenTO token = new AccessTokenTO();
         token.setLogin("anton.brueckner");
-        return new BearerTokenTO(null, null, 600, null, token,new HashSet<>());
+        return new BearerTokenTO(null, null, 600, null, token, new HashSet<>());
     }
 }
