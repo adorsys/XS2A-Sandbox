6850ac2d39940052554c4326350656bcb0401eb6
Manjari Kumari <mak@adorsys.de>
2021-02-23 00:05:01 +0100

    PSD2-412 TPP-UI. New user should be created at first page of user list

--- tpp-ui/src/app/components/admin/admins.component.ts
+++ tpp-ui/src/app/components/admin/admins.component.ts
@@ -1,23 +1,25 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {debounceTime, tap} from 'rxjs/operators';
-import {User, UserResponse} from '../../models/user.model';
-import {UserService} from '../../services/user.service';
-import {PageConfig, PaginationConfigModel,} from '../../models/pagination-config.model';
-import {TppManagementService} from '../../services/tpp-management.service';
-import {PageNavigationService} from '../../services/page-navigation.service';
-import {ActivatedRoute, Router} from '@angular/router';
-import {ADMIN_KEY} from '../../commons/constant/constant';
-import {InfoService} from '../../commons/info/info.service';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import {TppUserService} from '../../services/tpp.user.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { debounceTime, tap } from 'rxjs/operators';
+import { User, UserResponse } from '../../models/user.model';
+import { UserService } from '../../services/user.service';
+import {
+  PageConfig,
+  PaginationConfigModel,
+} from '../../models/pagination-config.model';
+import { TppManagementService } from '../../services/tpp-management.service';
+import { PageNavigationService } from '../../services/page-navigation.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ADMIN_KEY } from '../../commons/constant/constant';
+import { InfoService } from '../../commons/info/info.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { TppUserService } from '../../services/tpp.user.service';
 
 @Component({
   selector: 'app-admins',
   templateUrl: './admins.component.html',
   styleUrls: ['./admins.component.scss'],
 })
-
 export class AdminsComponent implements OnInit {
   admin: string;
   users: User[] = [];
@@ -42,8 +44,7 @@ export class AdminsComponent implements OnInit {
     private modalService: NgbModal,
     private router: Router,
     private userInfoService: TppUserService
-  ) {
-  }
+  ) {}
 
   ngOnInit() {
     this.admin = localStorage.getItem(ADMIN_KEY);
@@ -52,9 +53,7 @@ export class AdminsComponent implements OnInit {
   }
 
   pageChange(pageConfig: PageConfig) {
-    this.listAdmins(
-      pageConfig.pageNumber,
-      pageConfig.pageSize);
+    this.listAdmins(pageConfig.pageNumber, pageConfig.pageSize);
   }
 
   changePageSize(num: number): void {
@@ -62,16 +61,14 @@ export class AdminsComponent implements OnInit {
   }
 
   private getAdmins() {
-    this.listAdmins(
-      this.config.currentPageNumber,
-      this.config.itemsPerPage);
+    this.listAdmins(this.config.currentPageNumber, this.config.itemsPerPage);
   }
 
   private onQueryUsers() {
     this.searchForm.valueChanges
       .pipe(
         tap((val) => {
-          this.searchForm.patchValue(val, {emitEvent: false});
+          this.searchForm.patchValue(val, { emitEvent: false });
         }),
         debounceTime(750)
       )
@@ -81,43 +78,45 @@ export class AdminsComponent implements OnInit {
       });
   }
 
-  listAdmins(page: number, size: number,) {
+  listAdmins(page: number, size: number) {
     this.tppManagementService
       .getAllAdmins(page - 1, size)
       .subscribe((response: UserResponse) => {
         this.users = response.users;
+        this.users.reverse();
         this.config.totalItems = response.totalElements;
       });
-
   }
 
   openConfirmation(content, userId: string, type: string) {
-    this.modalService.open(content).result.then(
-      () => {
-        this.userInfoService.getUserInfo().subscribe((user: User) => {
-
-            if (type === 'delete') {
-              this.tppManagementService.deleteUser(userId).subscribe(() => {
-                if (userId === user.id) {
-                  localStorage.removeItem('access_token');
-                  this.router.navigateByUrl('/login');
-                } else {
-                  this.getAdmins();
-                }
-                this.infoService.openFeedback('Admin was successfully deleted!', {
-                  severity: 'info',
-                });
+    this.modalService.open(content).result.then(() => {
+      this.userInfoService.getUserInfo().subscribe(
+        (user: User) => {
+          if (type === 'delete') {
+            this.tppManagementService.deleteUser(userId).subscribe(() => {
+              if (userId === user.id) {
+                localStorage.removeItem('access_token');
+                this.router.navigateByUrl('/login');
+              } else {
+                this.getAdmins();
+              }
+              this.infoService.openFeedback('Admin was successfully deleted!', {
+                severity: 'info',
               });
-            } else if (type === 'pin') {
-              this.tppManagementService.changePin(userId, this.newPin).subscribe(() => {
+            });
+          } else if (type === 'pin') {
+            this.tppManagementService
+              .changePin(userId, this.newPin)
+              .subscribe(() => {
                 this.getAdmins();
                 this.infoService.openFeedback('Pin was successfully changed!', {
                   severity: 'info',
                 });
               });
-            }
-          },
-          () => {});
-      });
+          }
+        },
+        () => {}
+      );
+    });
   }
 }
