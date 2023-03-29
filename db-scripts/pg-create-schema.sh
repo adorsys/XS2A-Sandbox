8d50717ae364eeacd990caf2834a9b4bf1fcb633
Hryhorii Hevorkian <hhe@adorsys.com.ua>
2023-03-29 14:14:48 +0200

    added release notes.

--- db-scripts/pg-create-schema.sh
+++ db-scripts/pg-create-schema.sh
@@ -11,3 +11,14 @@ psql -U postgres -d consent -c 'CREATE SCHEMA IF NOT EXISTS ledgers AUTHORIZATIO
 
 echo "Create schema='tpp' for local postgres installation"
 psql -U postgres -d consent -c 'CREATE SCHEMA IF NOT EXISTS tpp AUTHORIZATION cms;'
+
+echo "Create keycloak database and user"
+echo "SELECT 'CREATE DATABASE keycloak' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'keycloak')\gexec" | psql
+if psql -t -c '\du' | cut -d \| -f 1 | grep -qw keycloak;
+ then
+  echo "User keycloak exists"
+else
+ echo "Create User keycloak"
+ psql -c "CREATE USER keycloak WITH PASSWORD 'keycloak';"
+ psql -c "GRANT ALL ON DATABASE keycloak TO keycloak;"
+fi
---
+++ docs/release_notes/Release_notes_6.6.md
@@ -0,0 +1,11 @@
+# Release notes v.6.6
+
+Current version consists of following components:
+
+1. **Ledgers v.4.xx**
+2. **XS2A v.14.xx**
+3. **ASPSP-Profile v.14.xx**
+4. **Consent-Management v.14.xx**
+5. **XS2A Connector-Examples v.14.xx**
+
+-   Fixed docker-compose files for the project.
